154de153d8584259124861815180bd321aa41843
hongjh <565983236@qq.com>
2018-05-25 10:29:48 +0800

    update

--- src/routes.php
+++ src/routes.php
@@ -10,10 +10,10 @@ $app->delete('/bank/accounts/{accounts}', 'BankController@destroy');
 $app->get('/bank/balance', 'BalanceController@show');
 
 // Withdraw money   取款
-$app->post('/bank/balance', 'BankController@destroy');
+$app->post('/bank/money/withdraw', 'BankController@destroy');
 
 // Deposit money    存款
-$app->post('/bank/balance', 'BankController@destroy');
+$app->post('/bank/money', 'BankController@destroy');
 
 // Transfer money (daily transfer limit of $10000 per account)
 // 转帐 (每日限额 $10000)
